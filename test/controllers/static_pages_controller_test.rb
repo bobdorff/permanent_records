5c2820cb9094370f5032014401b17a6375d64783
bobdorff <robertdorff@gmail.com>
2016-04-28 11:09:00 -0500

    write tests for proper titles on home and about

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Permanent Records | Home"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "Permanent Records | About"
   end
 
 end
