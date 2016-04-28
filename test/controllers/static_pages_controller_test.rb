f9b0f6b0d22b685dd211afdd78fb48ee73e01917
bobdorff <robertdorff@gmail.com>
2016-04-28 12:31:46 -0500

    complete basic footer styling

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Permanent Records | Home"
+    assert_select "title", "Permanent Records"
   end
 
   test "should get about" do
