e30c444d02ea9e3fa7296b4af4b6cb8731340d26
bobdorff <robertdorff@gmail.com>
2016-04-29 10:20:15 -0500

    update reg form to handle errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       #handle save
-      
+    else
+      render 'new'
+    end
+  end
 
   private
 
   def user_params
-
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
