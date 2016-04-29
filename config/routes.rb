0e71d143e6ddca1cf54184ce3b964c460b9b58a8
bobdorff <robertdorff@gmail.com>
2016-04-29 15:18:06 -0500

    complete login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  get 'about' => "static_pages#about"
   resources :users
+  get    "about"  => "static_pages#about"
+  get    "login"  => "sessions#new"
+  post   "login"  => "sessions#create"
+  delete "logout" => "sessions#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
