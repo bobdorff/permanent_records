2ae5a4b9088954f7ea47cf322b0dc7f5f8f6c167
bobdorff <robertdorff@gmail.com>
2016-04-29 10:13:53 -0500

    add create to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      #handle save
+      
+
+  private
+
+  def user_params
+
 end
