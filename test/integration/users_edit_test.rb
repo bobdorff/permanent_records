fecdcf4b0a9d97f958d77619b64bd88070837a0b
bobdorff <robertdorff@gmail.com>
2016-05-01 19:51:54 -0500

    add test for user update

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:jimmy)
+  end
+
+  test "unsuccessful edit attempt" do 
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), user: { username: "", 
+                                    email: "foo@invalid",
+                                    password: "foo",
+                                    password_confirmation: "bar" 
+                                  }
+    assert_template "users/edit"
+  end
+
+  test "successful edit attempt" do 
+    get edit_user_path(@user)
+    assert_template "users/edit"
+    patch user_path(@user), user: { username: 'davey',
+                                    email: 'valid@gmail.com',
+                                    password: "",
+                                    password_confirmation: "" 
+                                  }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.username, 'davey'
+    assert_equal @user.email, 'valid@gmail.com' 
+  end
+end
+
