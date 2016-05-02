850584098b6f58755616f1f4d960837eca2bea97
bobdorff <robertdorff@gmail.com>
2016-05-01 23:28:09 -0500

    add current_user? method and complete test for redirect if user tries to access disallowed page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   def setup
-    @user = users(:jimmy)
+    @user     = users(:jimmy)
+    @user_two = users(:kendrick)
   end
 
   test "should get new" do
@@ -20,5 +21,19 @@ class UsersControllerTest < ActionController::TestCase
     patch :update, id: @user, user: {username: @user.username, email: @user.email}
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+
+  test "redirects edit if a user tries to access a different user's edit page" do 
+    log_in_as(@user_two)
+    get :edit, id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "redirects update if a user submits edits for someone else" do 
+    log_in_as(@user_two)
+    patch :update, id: @user, user: { name: @user.username, email: @user.email }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
